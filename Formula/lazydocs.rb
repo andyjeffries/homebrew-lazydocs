1b3b049b731a5a0f35a91a4fc0bf14e941089755
Andy Jeffries <andy@andyjeffries.co.uk>
2026-01-28 14:24:09 +0000

    Add initial version

--- Formula/lazydocs.rb
+++ Formula/lazydocs.rb
@@ -1,17 +1,17 @@
 class Lazydocs < Formula
-  desc "Lazygit-style TUI for browsing DevDocs documentation offline"
-  homepage "https://github.com/andyjeffries/lazydocs"
-  url "https://github.com/andyjeffries/lazydocs/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
-  license "MIT"
-  head "https://github.com/andyjeffries/lazydocs.git", branch: "main"
+  desc 'Lazygit-style TUI for browsing DevDocs documentation offline'
+  homepage 'https://github.com/andyjeffries/lazydocs'
+  url 'https://github.com/andyjeffries/lazydocs/archive/refs/tags/v0.1.0.tar.gz'
+  sha256 '6441ec802048d7d57d6eaf721a36610d4940e22b87674b0b0cf90ae0ea716723'
+  license 'MIT'
+  head 'https://github.com/andyjeffries/lazydocs.git', branch: 'main'
 
-  depends_on "go" => :build
-  depends_on "sqlite"
+  depends_on 'go' => :build
+  depends_on 'sqlite'
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"),
-           "-tags", "sqlite_fts5", "./cmd/lazydocs"
+    system 'go', 'build', *std_go_args(ldflags: "-s -w -X main.version=#{version}"),
+           '-tags', 'sqlite_fts5', './cmd/lazydocs'
   end
 
   test do
