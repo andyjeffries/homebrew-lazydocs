a2e502435926273e16df0efa11b851b82c886381
Andy Jeffries <andy@andyjeffries.co.uk>
2026-01-28 14:20:45 +0000

    Initial commit

---
+++ Formula/lazydocs.rb
@@ -0,0 +1,20 @@
+class Lazydocs < Formula
+  desc "Lazygit-style TUI for browsing DevDocs documentation offline"
+  homepage "https://github.com/andyjeffries/lazydocs"
+  url "https://github.com/andyjeffries/lazydocs/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "PLACEHOLDER_SHA256"
+  license "MIT"
+  head "https://github.com/andyjeffries/lazydocs.git", branch: "main"
+
+  depends_on "go" => :build
+  depends_on "sqlite"
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"),
+           "-tags", "sqlite_fts5", "./cmd/lazydocs"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/lazydocs --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,24 @@
+# Homebrew Tap for LazyDocs
+
+This is the official Homebrew tap for [LazyDocs](https://github.com/andyjeffries/lazydocs).
+
+## Installation
+
+```bash
+brew tap andyjeffries/lazydocs
+brew install lazydocs
+```
+
+## Updating
+
+```bash
+brew update
+brew upgrade lazydocs
+```
+
+## Uninstalling
+
+```bash
+brew uninstall lazydocs
+brew untap andyjeffries/lazydocs
+```
